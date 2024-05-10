28df0dcabbc41f7e4e81117c909e67ecbe47247b
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-10 23:41:52 +0300

    Remove certain methods cause they will have direct implementation and are not necessary

--- Domain/JobCandidates/IJobCandidateRepository.cs
+++ Domain/JobCandidates/IJobCandidateRepository.cs
@@ -4,11 +4,5 @@ public interface IJobCandidateRepository
 {
     Task<JobCandidate?> GetByIdAsync(JobCandidateId id, CancellationToken cancellationToken = default);
 
-    Task<IEnumerable<JobCandidate>> GetJobCandidatesAsync(CancellationToken cancellationToken = default);
-
-    Task<IEnumerable<JobCandidate>> GetJobCandidatesPageAsync(int pageNo = 0, int pageSize = 10, CancellationToken cancellationToken = default);
-
     void Add(JobCandidate candidate);
-
-    void UpdateAsync(JobCandidate candidate, CancellationToken cancellationToken = default);
 }
