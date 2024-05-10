53d020e448d76f3df494b57d69380f35469c69a0
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-10 23:53:16 +0300

    Add the services to the wep api

---
+++ Host/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Host.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        using var dbContext = scope.ServiceProvider.GetRequiredService<CandidateContext>();
+
+        dbContext.Database.Migrate();
+    }
+}
--- Host/Program.cs
+++ Host/Program.cs
@@ -1,15 +1,20 @@
+using Application;
+using Host.Extensions;
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.ApplyMigrations();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
