412f80ca300462b452c290baaa2c9ada42dd50c1
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-10 22:13:33 +0300

    Add application abstraction and exceptions

---
+++ Application/Abstractions/Emails/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using Domain.JobCandidates;
+
+namespace Application.Abstractions.Emails;
+
+public interface IEmailService
+{
+    Task SendAsync(Email recipient, string subject, string body);
+}
---
+++ Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Common.Contract;
+using MediatR;
+
+namespace Application.Abstractions.Messaging;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+}
