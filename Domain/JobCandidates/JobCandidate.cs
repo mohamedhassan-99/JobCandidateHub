0b5fdf2634bcd4a42342a97787ad2a0083f020e8
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-10 19:14:56 +0300

    Modify candidate entity to imply DDD concepts

--- Domain/JobCandidates/JobCandidate.cs
+++ Domain/JobCandidates/JobCandidate.cs
@@ -1,15 +1,70 @@
 ﻿using Domain.Common.Contract;
+using Domain.JobCandidates.Events;
 
 namespace Domain.JobCandidates;
 
 public class JobCandidate : Entity<JobCandidateId>
 {
-    public required FirstName FirstName { get; set; }
-    public required LastName LastName { get; set; }
-    public PhoneNumber PhoneNumber { get; set; }
-    public required Email Email { get; set; }
-    public TimeInterval CallIn{ get; set; }
-    public URL LinkedIn { get; set; }
-    public URL GitHub { get; set; }
-    public required Comment Comment { get; set; }
+    private JobCandidate() { }
+    public FirstName FirstName { get; private set; }
+    public LastName LastName { get; private set; }
+    public PhoneNumber? PhoneNumber { get; private set; }
+    public Email Email { get; private set; }
+    public TimeInterval? CallIn { get; private set; }
+    public URL? LinkedIn { get; private set; }
+    public URL? GitHub { get; private set; }
+    public Comment Comment { get; private set; }
+
+    
+    public JobCandidate Update(JobCandidateId id,
+        FirstName? firstName,
+        LastName? lastName,
+        PhoneNumber? phone,
+        Email? email,
+        TimeInterval? callIn,
+        URL? linkedIn,
+        URL? gitHub,
+        Comment? comment)
+    {
+        FirstName = firstName ?? FirstName;
+        LastName = lastName ?? LastName;
+        PhoneNumber = phone ?? PhoneNumber;
+        Email = email ?? Email;
+        CallIn = callIn ?? CallIn;
+        LinkedIn = linkedIn ?? LinkedIn;
+        GitHub = gitHub ?? GitHub;
+        Comment = comment ?? Comment;
+
+        if(!GetDomainEvents().Any(a => a.GetType() ==  typeof(JobCandidateUpdatedEvent)))
+            RaiseDomainEvent(new JobCandidateUpdatedEvent(this));
+
+        return this;
+    }
+    public static JobCandidate Create(
+        FirstName firstName,
+        LastName lastName,
+        PhoneNumber phone,
+        Email email,
+        TimeInterval? callIn,
+        URL linkedIn,
+        URL gitHub,
+        Comment comment)
+    {
+        var jobCandidate = new JobCandidate()
+        {
+            Id = new(Guid.NewGuid()),
+            FirstName = firstName,
+            LastName = lastName,
+            PhoneNumber = phone,
+            Email = email,
+            CallIn = callIn,
+            LinkedIn = linkedIn,
+            GitHub = gitHub,
+            Comment = comment
+        };
+
+        jobCandidate.RaiseDomainEvent(new JobCandidateCreatedEvent(jobCandidate));
+
+        return jobCandidate;
+    }
 }
