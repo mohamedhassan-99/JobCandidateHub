2a44cad53f7fc79e6c928164c6d5f7d35d9cdc59
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-10 19:15:37 +0300

    Add job candidate repo interface

---
+++ Domain/JobCandidates/IJobCandidateRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.JobCandidates;
+
+public interface IJobCandidateRepository
+{
+    Task<JobCandidate?> GetByIdAsync(JobCandidateId id, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<JobCandidate>> GetJobCandidatesAsync(CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<JobCandidate>> GetJobCandidatesPageAsync(int pageNo = 0, int pageSize = 10, CancellationToken cancellationToken = default);
+
+    void Add(JobCandidate booking);
+
+    void UpdateAsync(JobCandidate booking, CancellationToken cancellationToken = default);
+}
