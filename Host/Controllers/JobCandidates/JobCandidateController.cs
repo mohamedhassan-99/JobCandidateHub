9dfc2f5c560f2447c80e48dcf4b04bc08b3e82a6
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-11 00:15:36 +0300

    Add job candidate controller

---
+++ Host/Controllers/JobCandidates/JobCandidateController.cs
@@ -0,0 +1,55 @@
+﻿using Application.JobCanidates.CreateCandidate;
+using Application.JobCanidates.GetCandidates;
+using Application.JobCanidates.UpdateCandidate;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host.Controllers.JobCandidates;
+
+[Route("api/[controller]")]
+[ApiController]
+public class JobCandidateController(ISender sender) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCandidates()
+    {
+        var candidates = sender.Send(new GetCandidatesQuery());
+
+        return Ok(candidates);
+    }
+
+    [HttpPatch]
+    public IActionResult CreateOrUpdateCandidates(JobCandidatePatchRequest request)
+    {
+        if (request.Id == null || request.Id == Guid.Empty)
+        {
+            var result = sender.Send(new CreateCandidateCommand(request.FirstName,
+                                                                request.LastName,
+                                                                request.PhoneNumber,
+                                                                request.Email,
+                                                                request.CallTo,
+                                                                request.CallFrom,
+                                                                request.LinkedIn,
+                                                                request.GitHub,
+                                                                request.Comment));
+
+            return Ok(result);
+        }
+        else
+        {
+            var result = sender.Send(new UpdateCandidateCommand(request.Id.Value,
+                                                                request.FirstName,
+                                                                request.LastName,
+                                                                request.PhoneNumber,
+                                                                request.Email,
+                                                                request.CallTo,
+                                                                request.CallFrom,
+                                                                request.LinkedIn,
+                                                                request.GitHub,
+                                                                request.Comment));
+
+            return Ok(result);
+        }
+
+    }
+}
---
+++ Host/Controllers/JobCandidates/JobCandidatePatchRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Host.Controllers.JobCandidates;
+
+public record JobCandidatePatchRequest
+{
+    public Guid? Id { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public string PhoneNumber { get; init; }
+    public string Email { get; init; }
+    public TimeOnly CallFrom { get; set; }
+    public TimeOnly CallTo { get; set; }
+    public string LinkedIn { get; init; }
+    public string GitHub { get; init; }
+    public string Comment { get; init; }
+}
