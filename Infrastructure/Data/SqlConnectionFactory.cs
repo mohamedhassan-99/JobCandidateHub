dab73ca360701bfebc2bb36d41dfd2fe93722fd9
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-10 23:45:23 +0300

    Implement the sql connection factory service

---
+++ Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using Application.Abstractions.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Infrastructure.Data;
+
+internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public SqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        var optionBuilder = new DbContextOptionsBuilder();
+
+        optionBuilder.UseSqlServer(_connectionString);
+        
+        var connection = new DbContext(optionBuilder.Options);
+
+        return connection.Database.GetDbConnection();
+    }
+}
