8b491844ef8a259e7e158016369fd8358d953b96
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-11 01:27:08 +0300

    Add Sql Mapper for time only

--- Infrastructure/DependancyInjection.cs
+++ Infrastructure/DependancyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Abstractions.Data;
 using Application.Abstractions.Emails;
+using Dapper;
 using Domain.Common.Contract;
 using Domain.JobCandidates;
 using Infrastructure.Configurations.DbContextConfiguration.Interceptors;
@@ -9,6 +10,7 @@ using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Data;
 
 namespace Infrastructure;
 
@@ -34,6 +36,22 @@ public static class DependancyInjection
 
         services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
 
+        SqlMapper.AddTypeHandler(new SqlTimeOnlyTypeHandler());
+
         return services;
+    }
+
+}
+
+public class SqlTimeOnlyTypeHandler : SqlMapper.TypeHandler<TimeOnly>
+{
+    public override void SetValue(IDbDataParameter parameter, TimeOnly time)
+    {
+        parameter.Value = time.ToString();
+    }
+
+    public override TimeOnly Parse(object value)
+    {
+        return TimeOnly.FromTimeSpan((TimeSpan)value);
     }
 }
