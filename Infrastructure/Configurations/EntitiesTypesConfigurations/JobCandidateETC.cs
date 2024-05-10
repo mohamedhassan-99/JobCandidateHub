8717e5bd133438107f0918c28b64bb15224059f0
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-11 00:18:49 +0300

    Modift the CallIn property configuration

--- Infrastructure/Configurations/EntitiesTypesConfigurations/JobCandidateETC.cs
+++ Infrastructure/Configurations/EntitiesTypesConfigurations/JobCandidateETC.cs
@@ -13,35 +13,39 @@ public class JobCandidateETC : IEntityTypeConfiguration<JobCandidate>
         builder.Property(candidate => candidate.Id)
             .HasConversion(candidateId => candidateId.Value, value => new JobCandidateId(value));
 
-        builder.OwnsOne(candidate => candidate.CallIn);
+        builder.OwnsOne(candidate => candidate.CallIn, ownedTypeBuilder =>
+        {
+            ownedTypeBuilder.Property(call => call.From).HasColumnName(nameof(JobCandidate.CallIn) + nameof(JobCandidate.CallIn.From));
+            ownedTypeBuilder.Property(call => call.To).HasColumnName(nameof(JobCandidate.CallIn) + nameof(JobCandidate.CallIn.To));
+        });
 
         builder.Property(candidate => candidate.FirstName)
             .IsRequired()
             .HasMaxLength(100)
             .HasConversion(name => name.Value, value => new FirstName(value));
-        
+
         builder.Property(candidate => candidate.LastName)
             .IsRequired()
             .HasMaxLength(100)
             .HasConversion(name => name.Value, value => new LastName(value));
-        
+
         builder.Property(candidate => candidate.PhoneNumber)
             .HasMaxLength(100)
             .HasConversion(name => name.Value, value => new PhoneNumber(value));
-        
+
         builder.Property(candidate => candidate.Email)
             .IsRequired()
             .HasMaxLength(200)
             .HasConversion(name => name.Value, value => new Email(value));
-        
+
         builder.Property(candidate => candidate.LinkedIn)
             .HasMaxLength(500)
             .HasConversion(name => name.Value, value => new URL(value));
-        
+
         builder.Property(candidate => candidate.GitHub)
             .HasMaxLength(500)
             .HasConversion(name => name.Value, value => new URL(value));
-        
+
         builder.Property(candidate => candidate.Comment)
             .IsRequired()
             .HasMaxLength(1500)
