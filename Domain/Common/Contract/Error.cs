5471d63804bde487124ffe0c7a664164246091de
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-05-10 22:14:44 +0300

    Add error model to be used in application messaging services

--- Domain/Common/Contract/Error.cs
+++ Domain/Common/Contract/Error.cs
@@ -5,4 +5,8 @@ public record Error(string Code, string Name)
     public static Error None = new(string.Empty, string.Empty);
 
     public static Error NullValue = new("Error.NullValue", "Null value was provided");
+    
+    public static Error NotFound = new("Error.NotFound", "Item not found");
+    
+    public static Error Concurrency = new("Error.Concurrency", "Item is not valid, try again");
 }
